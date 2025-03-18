06d23e1d37fd493827dae6b51a14a52c7d1c4f5a
maibui7879 <maibui7879@gmail.com>
2025-03-18 08:37:23 +0700

    gay

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 const productController = require("../controllers/productController");
 
-router.get("/", productController.getAll);
-router.get("/:id", productController.getById);
 router.get("/category/:category", productController.getByCategory);
 router.get("/search", productController.getByName);
+router.get("/:id", productController.getById);
+router.get("/", productController.getAll);
 router.post("/", productController.create);
 router.delete("/:id", productController.deleteById);
 
