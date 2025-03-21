361ca0346153a2e4a51a19207e17b825b9e7a744
maibui7879 <maibui7879@gmail.com>
2025-03-21 22:18:56 +0700

    gay

--- models/userModel.js
+++ models/userModel.js
@@ -2,34 +2,34 @@ const db = require("../config/db");
 
 const User = {
   getAll: async () => {
-    const [rows] = await db.query("SELECT id, ten, username, password, so_dien_thoai, email, ngay_sinh, dia_chi FROM Users");
+    const [rows] = await db.query("SELECT id, ten, username, password, so_dien_thoai, email, ngay_sinh, gioi_tinh, dia_chi FROM Users");
     return rows;
   },
 
   getById: async (id) => {
-    const [rows] = await db.query("SELECT id, ten, username, password, so_dien_thoai, email, ngay_sinh, dia_chi FROM Users WHERE id = ?", [id]);
+    const [rows] = await db.query("SELECT id, ten, username, password, so_dien_thoai, email, ngay_sinh, gioi_tinh, dia_chi FROM Users WHERE id = ?", [id]);
     return rows[0];
   },
 
   getByUsername: async (username) => {
-    const [rows] = await db.query("SELECT id, ten, username, password, so_dien_thoai, email, ngay_sinh, dia_chi FROM Users WHERE username = ?", [username]);
+    const [rows] = await db.query("SELECT id, ten, username, password, so_dien_thoai, email, ngay_sinh, gioi_tinh, dia_chi FROM Users WHERE username = ?", [username]);
     return rows[0];
   },
 
   create: async (data) => {
-    const { ten, username, password, so_dien_thoai, email, ngay_sinh, dia_chi } = data;
+    const { ten, username, password, so_dien_thoai, email, ngay_sinh, gioi_tinh, dia_chi } = data;
     const [result] = await db.query(
-      "INSERT INTO Users (ten, username, password, so_dien_thoai, email, ngay_sinh, dia_chi) VALUES (?, ?, ?, ?, ?, ?, ?)",
-      [ten, username, password, so_dien_thoai, email, ngay_sinh, dia_chi]
+      "INSERT INTO Users (ten, username, password, so_dien_thoai, email, ngay_sinh, gioi_tinh, dia_chi) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+      [ten, username, password, so_dien_thoai, email, ngay_sinh, gioi_tinh, dia_chi]
     );
     return result.insertId;
   },
 
   update: async (id, data) => {
-    const { ten, username, password, so_dien_thoai, email, ngay_sinh, dia_chi } = data;
+    const { ten, username, password, so_dien_thoai, email, ngay_sinh, gioi_tinh, dia_chi } = data;
     const [result] = await db.query(
-      "UPDATE Users SET ten=?, username=?, password=?, so_dien_thoai=?, email=?, ngay_sinh=?, dia_chi=? WHERE id=?",
-      [ten, username, password, so_dien_thoai, email, ngay_sinh, dia_chi, id]
+      "UPDATE Users SET ten=?, username=?, password=?, so_dien_thoai=?, email=?, ngay_sinh=?, gioi_tinh=?, dia_chi=? WHERE id=?",
+      [ten, username, password, so_dien_thoai, email, ngay_sinh, gioi_tinh, dia_chi, id]
     );
     return result.affectedRows;
   },
