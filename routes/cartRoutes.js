46094143c7926dd64f099619dfe39cb0510f2168
maibui7879 <maibui7879@gmail.com>
2025-03-24 02:29:12 +0700

    a

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -5,7 +5,6 @@ const cartController = require("../controllers/cartController");
 router.get("/", cartController.getAll);
 router.get("/:userId", cartController.getByUserId);
 router.post("/", cartController.addToCart);
-router.delete("/:userId", cartController.deleteCartByUser);
 router.delete("/", cartController.removeProduct);
 router.delete("/:id", cartController.deleteById);
 router.get("/item/:id", cartController.getById);
