965906c457972fd35e42154c6ec6ba2b9d4f1b75
maibui7879 <maibui7879@gmail.com>
2025-03-24 15:14:27 +0700

    a

--- models/orderModel.js
+++ models/orderModel.js
@@ -27,24 +27,27 @@ const Order = {
   },
 
   getByUserPaymentTransport: async (userId, transported, paid) => {
-    const [rows] = await db.query("SELECT * FROM Don_Hang WHERE id_khach_hang = ? AND da_van_chuyen = ? AND da_thanh_toan = ?", [userId, transported, paid]);
+    const [rows] = await db.query(
+      "SELECT * FROM Don_Hang WHERE id_khach_hang = ? AND da_van_chuyen = ? AND da_thanh_toan = ?",
+      [userId, transported, paid]
+    );
     return rows;
   },
 
   create: async (data) => {
-    const { id_khach_hang, ngay_thanh_toan, list_hang, gia_tien, da_van_chuyen, da_thanh_toan } = data;
+    const { id_khach_hang, ngay_thanh_toan, list_hang, gia_tien, da_van_chuyen, da_thanh_toan, dia_chi_nhan_hang } = data;
     const [result] = await db.query(
-      "INSERT INTO Don_Hang (id_khach_hang, ngay_thanh_toan, list_hang, gia_tien, da_van_chuyen, da_thanh_toan) VALUES (?, ?, ?, ?, ?, ?)",
-      [id_khach_hang, ngay_thanh_toan, JSON.stringify(list_hang), gia_tien, da_van_chuyen, da_thanh_toan]
+      "INSERT INTO Don_Hang (id_khach_hang, ngay_thanh_toan, list_hang, gia_tien, da_van_chuyen, da_thanh_toan, dia_chi_nhan_hang) VALUES (?, ?, ?, ?, ?, ?, ?)",
+      [id_khach_hang, ngay_thanh_toan, JSON.stringify(list_hang), gia_tien, da_van_chuyen, da_thanh_toan, dia_chi_nhan_hang]
     );
     return result.insertId;
   },
 
   update: async (id, data) => {
-    const { ngay_thanh_toan, list_hang, gia_tien, da_van_chuyen, da_thanh_toan } = data;
+    const { ngay_thanh_toan, list_hang, gia_tien, da_van_chuyen, da_thanh_toan, dia_chi_nhan_hang } = data;
     const [result] = await db.query(
-      "UPDATE Don_Hang SET ngay_thanh_toan=?, list_hang=?, gia_tien=?, da_van_chuyen=?, da_thanh_toan=? WHERE id=?",
-      [ngay_thanh_toan, JSON.stringify(list_hang), gia_tien, da_van_chuyen, da_thanh_toan, id]
+      "UPDATE Don_Hang SET ngay_thanh_toan=?, list_hang=?, gia_tien=?, da_van_chuyen=?, da_thanh_toan=?, dia_chi_nhan_hang=? WHERE id=?",
+      [ngay_thanh_toan, JSON.stringify(list_hang), gia_tien, da_van_chuyen, da_thanh_toan, dia_chi_nhan_hang, id]
     );
     return result.affectedRows;
   },
